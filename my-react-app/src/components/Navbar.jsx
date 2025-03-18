4203230637def86b687897429e8f5d876e5be732
Phpar-Ph <146304604+Phpar-Ph@users.noreply.github.com>
2025-03-18 23:46:44 +0800

    install dependencies

---
+++ my-react-app/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="h-14 w-full bg-amber-400">
+      <h1 className=" ">THIS IS NAVBAR</h1>
+    </div>
+  );
+}
+
+export default Navbar;
